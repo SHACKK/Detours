1519b0f2e60fb9a4faef556b1625c942e1f1ee29
SHACKK <jfhg456@kakao.com>
2021-08-22 02:38:28 +0900

    Hooking practice

---
+++ victim/Src/BegavuirBasedEngine/ExportFunc.cpp
@@ -0,0 +1,7 @@
+#include "pch.h"
+#include "ExportFunc.h"
+
+void DummyAPI(void)
+{
+
+}
---
+++ victim/Src/victim/main.cpp
@@ -0,0 +1,14 @@
+#include "stdafx.h"
+
+int main(void)
+{
+    SYSTEMTIME stTime;
+    GetLocalTime(&stTime);
+
+    printf("%04d-%02d-%02d %02d:%02d:%02d\n",
+        stTime.wYear, stTime.wMonth, stTime.wDay,
+        stTime.wHour, stTime.wMinute, stTime.wSecond);
+
+    Sleep(100 * 1000);
+    return 0;
+}
