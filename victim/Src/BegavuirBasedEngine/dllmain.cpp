9d17e198e2c6efd54d4df2ee93d8347f43c39338
SHACKK <jfhg456@kakao.com>
2021-08-22 02:31:54 +0900

    Hooking practice

---
+++ victim/Src/BegavuirBasedEngine/dllmain.cpp
@@ -0,0 +1,33 @@
+﻿#include "pch.h"
+
+static LONG dwSlept = 0;
+
+static VOID(WINAPI* True_Sleep)(DWORD dwMilliseconds) = Sleep;
+
+VOID WINAPI Hooked_Sleep(DWORD dwMilliseconds)
+{
+    printf("Let's say slept %us(ms).\n", dwMilliseconds);
+}
+
+BOOL WINAPI DllMain(HINSTANCE hinst, DWORD dwReason, LPVOID reserved)
+{
+    if (DetourIsHelperProcess()) {
+        return TRUE;
+    }
+
+    if (dwReason == DLL_PROCESS_ATTACH) {
+        DetourRestoreAfterWith();
+
+        DetourTransactionBegin();
+        DetourUpdateThread(GetCurrentThread());
+        DetourAttach(&(PVOID&)True_Sleep, Hooked_Sleep);
+        DetourTransactionCommit();
+    }
+    else if (dwReason == DLL_PROCESS_DETACH) {
+        DetourTransactionBegin();
+        DetourUpdateThread(GetCurrentThread());
+        DetourDetach(&(PVOID&)True_Sleep, Hooked_Sleep);
+        DetourTransactionCommit();
+    }
+    return TRUE;
+}
